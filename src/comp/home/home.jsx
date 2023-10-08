01afcdbe04f21911cd9bd3370f6f7f8caa6e357f
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-08 13:28:04 +0530

    the modifies jsx file updated

--- src/comp/home/home.jsx
+++ src/comp/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GrowingPlant from '../growingplant/growingPlant'
 
 function Home() {
+  console.log('Home page running fine');
   return (
     <div>
         <h1>My personal info</h1>
