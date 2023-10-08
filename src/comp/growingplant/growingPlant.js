29bb60bb140a786f84b78493950e0af5dc04db59
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-08 15:05:50 +0530

    Object and Object destructering in jsx

--- src/comp/growingplant/growingPlant.js
+++ src/comp/growingplant/growingPlant.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import './GrowingPlant.scss'
 
-function GrowingPlant() {
 
-  const plantData = {
-    name : 'Rose',
-    color: 'red',
-    backgroundColor: 'green',
-    icon : '🌹',
-    size : 30
-  }
+//Using Props
+function GrowingPlant(props) {
+  
+  console.log('props working fine');
+
+  //objects
+  const defaultData = {
+    name: 'Default Name',
+    size: '1em',
+    backgroundColor: 'red',
+    icon: '🌱'
+  };
+
+  //object destructering
+  const { plantData = defaultData} = props;
 
   return (
     <div >
       <h1>I'm {plantData.name} <span style={{fontSize:  `${plantData.size}`,backgroundColor: `${plantData.backgroundColor}`}}>{plantData.icon}</span></h1>
     </div>
   )
+  
 }
 
 export default GrowingPlant
