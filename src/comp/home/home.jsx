2d63841ab49c78504a9042ae6fdeac61a8f439be
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-08 15:05:06 +0530

    Creating dynamic code using props

--- src/comp/home/home.jsx
+++ src/comp/home/home.jsx
@@ -2,13 +2,34 @@ import React from 'react'
 import GrowingPlant from '../growingplant/growingPlant'
 
 function Home() {
-  console.log('Home page running fine');
+
+  console.log('Home page running fine'); 
+  
+  const plantData1 = {
+    name : 'Rose',
+    color: 'red',
+    backgroundColor: 'green',
+    icon : '🌹',
+    size : 30
+  }
+
+  const plantData2 = {
+    name : 'Sunflower',
+    color: 'Yellow',
+    backgroundColor: 'green',
+    icon : '🌻',
+    size : 30
+  }
+
   return (
     <div>
         <h1>My personal info</h1>
+        <GrowingPlant plantData={plantData1}/>
+        <GrowingPlant plantData={plantData2}/>
         <GrowingPlant />
     </div>
   )
+  
 }
 
 export default Home
