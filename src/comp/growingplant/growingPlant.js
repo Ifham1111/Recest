a8f98af6422a4440dce30595a41972005694af1b
Mohamed MRI <mohamedmri.21@uom.lk>
2023-10-10 11:53:53 +0530

    React Js finished

--- src/comp/growingplant/growingPlant.js
+++ src/comp/growingplant/growingPlant.js
@@ -1,7 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import "./GrowingPlant.scss";
 
-//Using Props
+// const Hi = () => {
+//   useEffect(()=> {
+//     console.log("Hi component mounted");
+//     return () => console.log("Hi component unmounted");
+//   },[]);
+
+//   return <div>test</div>
+
+// }
+
+// Using Props
 function GrowingPlant(props) {
   // console.log("props working fine");
 
@@ -25,18 +35,20 @@ function GrowingPlant(props) {
   const [dynamicSize, setdynamicSize] = useState(size);
   // console.log("dynamic Size", dynamicSize);
 
+  // const [displayHi, setdisplayHi] = useEffect(false);
+
   //useEffect
   //in first parameter we have to give parametere and in second parametre dependancies array when the dependancy array empty it'll run only the first time when the the page load if we are giving the dipendancies then the componetns monitor that dipendancies according to aour condition
-  useEffect(() => {
-    console.log("My code..");
-  }, []);
+  // useEffect(() => {
+  //   console.log("My code..");
+  // }, []);
 
-  useEffect(() => {
-    console.log("dynamicSize", dynamicSize);
-    if (dynamicSize > 100) {
-      alert("Plant grown sucessfully");
-    }
-  }, [dynamicSize]);
+  // useEffect(() => {
+  //   console.log("dynamicSize", dynamicSize);
+  //   if (dynamicSize > 100) {
+  //     alert("Plant grown sucessfully");
+  //   }
+  // }, [dynamicSize]);
 
   useEffect(() => {
     //set timeout, set interval, clear inteerval
@@ -45,7 +57,7 @@ function GrowingPlant(props) {
         setdynamicSize(dynamicSize + 2);
       }
     }, 1000);
-    
+
     return () => clearInterval(interval);
 
   }, [dynamicSize, name]);
@@ -58,7 +70,7 @@ function GrowingPlant(props) {
 
   //life cycle of react >> (when that all start) mount , unmount, update
 
-  // when the component is happen defaultly the rturn of the components happen then when we using hoooks specially useEfeect without dependencies thats mean when first time components mount that it is want to happen 
+  // when the component is happen defaultly the rturn of the components happen then when we using hoooks specially useEfeect without dependencies thats mean when first time components mount that it is want to happen
 
   //when the components unmount that time we have to specify the return what should be happening next
 
@@ -86,5 +98,48 @@ function GrowingPlant(props) {
     </div>
   );
 }
+
+// class GrowingPlant extends Component {
+//   constructor(props) {
+//     super(props);
+    
+//     this.state = {
+//       dynamicSize: props.plantData.size
+//     }
+//   };
+
+//   handleClick = () => {
+//     this.setState({dynamicSize: this.dynamicSize + 20});
+//   };
+
+//   render() {
+//     const{ plantData}=this.props;
+//     let { name, size, icon, backgroundColor, aboutMe } = plantData;
+
+
+//     return (
+//       <div>
+//         <h1>
+//           I'm {name}
+//           {name === "Rose" ? (
+//             <h6>This is a Rose</h6>
+//           ) : (
+//             <h6>This is a Sun flower</h6>
+//           )}
+//           <span
+//             style={{
+//               fontSize: `${dynamicSize}px`,
+//               backgroundColor: `${backgroundColor}`,
+//             }}
+//           >
+//             {icon}
+//           </span>
+//         </h1>
+
+//         <button onClick={this.handleClick}>Givewater</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default GrowingPlant;
